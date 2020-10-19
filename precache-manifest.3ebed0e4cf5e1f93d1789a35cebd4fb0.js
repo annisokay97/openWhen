0f9ac78082e7cfe963dd5942e5b0f42a4b1e9dd6
Hanna Mekshun <Hanna.Mekshun@tx.local>
2020-10-19 18:10:56 +0300

    Updates

---
+++ precache-manifest.3ebed0e4cf5e1f93d1789a35cebd4fb0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ab47fa49a69d30b1678311f683c859",
+    "url": "/openWhen/index.html"
+  },
+  {
+    "revision": "6e4d137cee24e6b66e58",
+    "url": "/openWhen/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "61de37dc93643a9142af",
+    "url": "/openWhen/static/css/main.5571613c.chunk.css"
+  },
+  {
+    "revision": "6e4d137cee24e6b66e58",
+    "url": "/openWhen/static/js/2.43cf0fc7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/openWhen/static/js/2.43cf0fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61de37dc93643a9142af",
+    "url": "/openWhen/static/js/main.8d60656d.chunk.js"
+  },
+  {
+    "revision": "24de2136ec69353d4bd5",
+    "url": "/openWhen/static/js/runtime-main.8610cc4f.js"
+  },
+  {
+    "revision": "069bb14e5c5f336eb123861f077cc2d8",
+    "url": "/openWhen/static/media/heart.069bb14e.png"
+  },
+  {
+    "revision": "8b5a7ac617f58107a219b745e0ebbe59",
+    "url": "/openWhen/static/media/modal.8b5a7ac6.png"
+  },
+  {
+    "revision": "93392ae4819d8962ae12892a3e6dcb59",
+    "url": "/openWhen/static/media/photo.93392ae4.jpg"
+  }
+]);
